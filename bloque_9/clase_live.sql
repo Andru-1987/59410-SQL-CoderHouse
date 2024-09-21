a5a48680379d2411fa4a2db7e6cfb168a56c4457
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-09-21 13:37:45 -0300

    update clase bloque 9

--- bloque_9/clase_live.sql
+++ bloque_9/clase_live.sql
@@ -79,3 +79,167 @@ UPDATE nautilus.CLIENTE
 
 DELETE FROM nautilus.CLIENTE
 	WHERE email = 'cliente3@example.com';
+    
+    
+    
+-- TRIGGERS DE AUDITORIA
+CREATE TABLE nautilus.log_cambio_vehiculo
+	(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        id_vehiculo INT ,
+		fecha_vehiculo DATE,
+		modelo VARCHAR(200),
+        tipo_vehiculo VARCHAR(200),
+        usuario_cambio VARCHAR(200),
+        fecha_cuando_cambio DATETIME DEFAULT (CURRENT_TIMESTAMP),
+        tipo_modificacion ENUM('BORRADO','ACTUALIZADO')
+    );
+
+CREATE TABLE nautilus.log_cambio_vehiculo_verbose
+	(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        id_vehiculo INT ,
+        id_vehiculo_viejo INT ,
+		fecha_vehiculo DATE,
+        fecha_vehiculo_viejo DATE,
+		modelo VARCHAR(200),
+        modelo_viejo VARCHAR(200),
+        tipo_vehiculo VARCHAR(200),
+        tipo_vehiculo_viejo VARCHAR(200),
+        usuario_cambio VARCHAR(200),
+        fecha_cuando_cambio DATETIME DEFAULT (CURRENT_TIMESTAMP),
+        tipo_modificacion ENUM('BORRADO','ACTUALIZADO')
+    );
+
+
+DROP TRIGGER IF EXISTS nautilus.after_update_vehiculo;
+DROP TRIGGER IF EXISTS nautilus.after_update_vehiculo_verbose;
+
+DELIMITER //
+CREATE 
+	TRIGGER nautilus.after_update_vehiculo
+    AFTER UPDATE ON nautilus.VEHICULO
+    FOR EACH ROW
+BEGIN
+	INSERT INTO 
+    nautilus.log_cambio_vehiculo
+    (
+	 id_vehiculo
+	,fecha_vehiculo
+	,modelo
+	,tipo_vehiculo
+	,usuario_cambio
+	,tipo_modificacion)
+    VALUES
+    (
+	 OLD.id_vehiculo
+    ,OLD.fecha_vehiculo
+    ,OLD.modelo
+    ,OLD.tipo_vehiculo
+    ,SESSION_USER()
+    ,2
+    );
+END //
+
+
+DELIMITER //
+CREATE 
+	TRIGGER nautilus.after_delete_vehiculo
+    AFTER DELETE ON nautilus.VEHICULO
+    FOR EACH ROW
+BEGIN
+	INSERT INTO 
+    nautilus.log_cambio_vehiculo
+    (
+	 id_vehiculo
+	,fecha_vehiculo
+	,modelo
+	,tipo_vehiculo
+	,usuario_cambio
+	,tipo_modificacion)
+    VALUES
+    (
+	 OLD.id_vehiculo
+    ,OLD.fecha_vehiculo
+    ,OLD.modelo
+    ,OLD.tipo_vehiculo
+    ,SESSION_USER()
+    ,1
+    );
+END //
+    
+DELIMITER ;
+    
+DELIMITER //
+CREATE 
+	TRIGGER nautilus.after_update_vehiculo_verbose
+    AFTER UPDATE ON nautilus.VEHICULO
+    FOR EACH ROW
+BEGIN
+	INSERT INTO 
+    nautilus.log_cambio_vehiculo_verbose
+    (
+	 id_vehiculo
+	,id_vehiculo_viejo
+	,fecha_vehiculo
+    ,fecha_vehiculo_viejo
+	,modelo
+    ,modelo_viejo
+	,tipo_vehiculo
+    ,tipo_vehiculo_viejo
+	,usuario_cambio
+	,tipo_modificacion)
+    VALUES
+    (
+		NEW.id_vehiculo
+    ,OLD.id_vehiculo
+	,NEW.fecha_vehiculo
+    ,OLD.fecha_vehiculo
+    ,NEW.modelo
+    ,OLD.modelo
+    ,NEW.tipo_vehiculo
+    ,OLD.tipo_vehiculo
+    ,SESSION_USER()
+    ,2
+    );
+END //
+    
+    
+DELIMITER ;
+
+
+SELECT * FROM nautilus.VEHICULO;
+SELECT * FROM nautilus.log_cambio_vehiculo;
+
+SELECT * FROM nautilus.log_cambio_vehiculo_verbose;
+
+SELECT
+		v.*
+   ,	lv.* 
+FROM nautilus.VEHICULO AS v
+JOIN nautilus.log_cambio_vehiculo AS lv
+	ON v.id_vehiculo = lv.id_vehiculo;
+
+
+
+UPDATE nautilus.VEHICULO
+SET fecha_vehiculo = '2023-10-10'
+WHERE id_vehiculo = 6;
+
+UPDATE nautilus.VEHICULO
+SET fecha_vehiculo = NOW()
+,	id_vehiculo = 50
+WHERE id_vehiculo = 10;
+
+
+DELETE FROM nautilus.VEHICULO;
+
+INSERT INTO nautilus.VEHICULO
+SELECT
+	id_vehiculo
+,	fecha_vehiculo
+,	modelo
+,	tipo_vehiculo
+FROM nautilus.log_cambio_vehiculo
+WHERE tipo_modificacion = 1
+;
